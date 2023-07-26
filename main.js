dee0389bd0507af7c1c4ad97298c481bd4c0ea43
jmasciar82 <109538968+jmasciar82@users.noreply.github.com>
2023-07-26 16:59:45 -0300

    Ejercicio 6

---
+++ main.js
@@ -0,0 +1,34 @@
+//😺 😸 😹 🐈 🐾 ⬛
+console.log('/* ---------------------------------*/ ')
+console.log('/*            Ejercicio a           */ ')
+console.log('/* ---------------------------------*/ ')
+var cantidadDeGatos  = 10 
+var x = 0
+while (x <= 10) {
+    x++    
+    console.log(`Gato #${x}: 😺`);
+    if(x == cantidadDeGatos ) break
+    x++    
+    console.log(`Gato #${x}: 😸`);
+    if(x == cantidadDeGatos ) break
+    x++
+    console.log(`Gato #${x}: 😹`);
+    if(x == cantidadDeGatos ) break
+
+}
+console.log('/* ---------------------------------*/ ')
+console.log('/*            Ejercicio b           */ ')
+console.log('/* ---------------------------------*/ ')
+
+var cantidadDeGatos = 5;
+var cantidadDePasos = 3;
+var emoji = ""
+
+for(x = 1; x <= cantidadDeGatos; x++) {
+    
+    for(i = 1; i <= cantidadDePasos; i++) {
+        emoji += "🐾"        
+    }
+    console.log(`Gato #${x}: 🐈` + emoji)
+    emoji = ''    
+}
